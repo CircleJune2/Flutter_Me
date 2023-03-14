687d253a174c592a84be52a480396946a7d3dfbf
wjlee <dearjune7@gmail.com>
2023-03-14 17:57:46 +0900

    초기세팅

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
 
+  WebViewController? controller;
 
   HomeScreen({Key? key}) : super(key: key);
 
